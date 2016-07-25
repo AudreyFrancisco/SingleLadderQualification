0b065762641d2f9c40d7114da9aa71c6a278ec82
Jacobus van Hoorne <j.w.van.hoorne@cern.ch>
2016-07-25 23:04:49 +0200

    added some members

--- TBoardConfig.h
+++ TBoardConfig.h
@@ -1,12 +1,28 @@
 #ifndef BOARDCONFIG_H
 #define BOARDCONFIG_H
 
+
+
+
+
 class TBoardConfig {
  private:
+  bool fTriggerEnable;
+  bool fPulseEnable;
+  int  fNTriggers;
+  int  fTriggerDelay;
+  int  fPulseDelay;
+  int  fTriggerSource;
+
+
  protected:
+
  public:
   TBoardConfig(const char *fName = 0, int boardIndex = 0) {};
+  
   virtual int GetBoardType() = 0;
+
+
 };
 
 
