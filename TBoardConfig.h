583f312a308fbbf12f266bacd7edce32d01f24e2
Jacobus van Hoorne <j.w.van.hoorne@cern.ch>
2016-07-28 17:53:51 +0200

    cleaned from redundancies with TBoardConfigDAQ

--- TBoardConfig.h
+++ TBoardConfig.h
@@ -1,26 +1,42 @@
 #ifndef BOARDCONFIG_H
 #define BOARDCONFIG_H
 
+#include "stdint.h"
 
 
 
 
 class TBoardConfig {
  private:
+
+ protected:
   bool fTriggerEnable;
   bool fPulseEnable;
   int  fNTriggers;
-  int  fTriggerDelay;
+  uint32_t  fTriggerDelay;
   int  fPulseDelay;
   int  fTriggerSource;
-
-
- protected:
 
  public:
   TBoardConfig(const char *fName = 0, int boardIndex = 0) {};
   
   virtual int GetBoardType() = 0;
+
+  bool GetTriggerEnable()             {return fTriggerEnable;};
+  bool GetPulseEnable()               {return fPulseEnable;};
+  int  GetNTriggers()                 {return fNTriggers;};
+  int  GetTriggerDelay()              {return fTriggerDelay;};
+  int  GetPulseDelay()                {return fPulseDelay;};
+  int  GetTriggerSource()             {return fTriggerSource;};
+
+
+  void SetTriggerEnable(bool trigEnable)             {fTriggerEnable = trigEnable;};
+  void SetPulseEnable  (bool pulseEnable)            {fPulseEnable   = pulseEnable;};
+  void SetNTriggers    (int nTriggers)               {fNTriggers     = nTriggers;};
+  void SetTriggerDelay (int trigDelay)               {fTriggerDelay  = trigDelay;};
+  void SetPulseDelay   (int pulseDelay)              {fPulseDelay    = pulseDelay;};
+  void SetTriggerSource(int trigSource)              {fTriggerSource = trigSource;};
+
 
 
 };
