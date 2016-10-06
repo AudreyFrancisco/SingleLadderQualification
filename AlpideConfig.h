fc6234f72e963060c2ec9e64c74947a915dde8c1
palpidefs <palpidefs@pb-d-128-141-125-147.cern.ch>
2016-10-06 16:52:49 +0200

    AlpideConfig: Added default value for config

--- AlpideConfig.h
+++ AlpideConfig.h
@@ -14,7 +14,7 @@ namespace AlpideConfig {
   void ConfigureFromu     (TAlpide *chip, Alpide::TPulseType pulseType, bool testStrobe, TChipConfig *config = 0);
   void ConfigureBuffers   (TAlpide *chip, TChipConfig *config = 0);
   void ConfigureCMU       (TAlpide *chip, TChipConfig *config = 0);
-  void WriteControlReg    (TAlpide *chip, Alpide::TChipMode chipMode, TChipConfig *config);
+  void WriteControlReg    (TAlpide *chip, Alpide::TChipMode chipMode, TChipConfig *config = 0);
   void BaseConfigPLL      (TAlpide *chip);
   void BaseConfigMask     (TAlpide *chip);
   void BaseConfigFromu    (TAlpide *chip);
