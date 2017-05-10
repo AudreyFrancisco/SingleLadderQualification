e9f189e7aa521b4f3191825a91e9414e3e0785ca
palpidefs <palpidefs@pb-d-128-141-125-147.cern.ch>
2017-05-10 10:31:09 +0200

    Removed debug output

--- TScan.cpp
+++ TScan.cpp
@@ -18,8 +18,6 @@ TScan::TScan (TScanConfig *config, std::vector <TAlpide *> chips, std::vector <T
   time_t       t = time(0);   // get time now
   struct tm *now = localtime( & t );
   sprintf(config->GetfNameSuffix(), "%02d%02d%02d_%02d%02d%02d", now->tm_year - 100, now->tm_mon + 1, now->tm_mday, now->tm_hour, now->tm_min, now->tm_sec);
-
-  std::cout << "filename suffix = " << config->GetfNameSuffix() << std::endl;
 }
 
 
