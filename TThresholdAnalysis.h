48a75545caa0b6d9cd78c5d0cf63e9c833909b29
Marcello Borri <marcello.borri@cern.ch>
2017-05-30 12:09:54 +0100

    Using TScanResultChip and TScanResult

--- TThresholdAnalysis.h
+++ TThresholdAnalysis.h
@@ -2,35 +2,121 @@
 #define TTHRESHOLDANALYSIS_H
 
 #include <deque>
+#include <map>
 #include <mutex>
+#include <stdio.h>
 #include <string>
 #include <time.h>
 #include <vector>
 
+#include "Common.h"
+
 #include "TScanAnalysis.h"
-#include "TScanConfig.h"
-#include "TScan.h"
+
+class THisto;
+class TScan;
+class TScanConfig;
+class TScanHisto;
 
 class TF1;
 class TGraph;
 
 class TThresholdResultChip : public TScanResultChip {
+ private:
+  
+  unsigned int m_boardIndex; 
+  unsigned int m_dataReceiver;
+  unsigned int m_chipId;
+  
+  int m_vPulseL;    //?
+  int m_vPulseH;    //?
+  int m_vPulseStep; //?
+  int m_nMask;      //?
+  
+  int m_counterPixelsNoHits;
+  int m_counterPixelsStuck;
+  int m_counterPixelsNoThreshold;
+  
+  double m_thresholdMean;
+  double m_thresholdStdDev;
+  double m_noiseMean;
+  double m_noiseStdDev;
+  
+  FILE* m_fileSummary;
+  FILE* m_filePixelNoHits;
+  FILE* m_filePixelStuck;
+  FILE* m_filePixelNoThreshold; // To do, based on chi2 cut?
+  FILE* m_filePixelFitResult;
+  FILE* m_fileRawData; 
+  
  public: 
-  TThresholdResultChip () : TScanResultChip () {};
+  TThresholdResultChip ();
+  ~TThresholdResultChip();
+  
+  void SetBoardIndex   (unsigned int aBoardIndex); 
+  void SetDataReceiver (unsigned int aDataReceiver);
+  void SetChipId       (unsigned int aChipId);
+  
+  void SetVPulseL (int aVPulseL);    
+  void SetVPulseH (int aVPulseH);    
+  void SetVPulseStep (int aVPulseStep); 
+  void SetNMask   (int aNMask);      
+  
+  void SetCounterPixelsNoHits      (int aCounterPixelsNoHits);
+  void SetCounterPixelsStuck       (int aCounterPixelsStuck);
+  void SetCounterPixelsNoThreshold (int aCounterPixelsNoThreshold);
+  
+  void SetThresholdMean   (double aThresholdMean);
+  void SetThresholdStdDev (double aThresholdStdDev);
+  void SetNoiseMean       (double aNoiseMean);
+  void SetNoiseStdDev     (double aNoiseStdDev);
+  
+  void SetFileSummary          (FILE* aFileName); 
+  void SetFilePixelNoHits      (FILE* aFileName);
+  void SetFilePixelStuck       (FILE* aFileName);
+  void SetFilePixelNoThreshold (FILE* aFileName); 
+  void SetFilePixelFitResult   (FILE* aFileName);
+  void SetFileRawData          (FILE* aFileName); 
+  
+  unsigned int GetBoardIndex   (); 
+  unsigned int GetDataReceiver ();
+  unsigned int GetChipId ();
+  
+  int GetVPulseL ();
+  int GetVPulseH ();
+  int GetVPulseStep ();
+  int GetNMask   ();
+  
+  int GetCounterPixelsNoHits      ();
+  int GetCounterPixelsStuck       ();
+  int GetCounterPixelsNoThreshold ();
+  
+  double GetThresholdMean   ();
+  double GetThresholdStdDev ();
+  double GetNoiseMean       ();
+  double GetNoiseStdDev     ();
+  
+  FILE* GetFileSummary          ();
+  FILE* GetFilePixelNoHits      ();
+  FILE* GetFilePixelStuck       ();
+  FILE* GetFilePixelNoThreshold (); 
+  FILE* GetFilePixelFitResult   ();
+  FILE* GetFileRawData          (); 
+  
 };
-
 
 class TThresholdResult : public TScanResult {
+  
  public: 
-  TThresholdResult () : TScanResult () {};
+  TThresholdResult  ();
+  ~TThresholdResult ();
+  
 };
-
 
 class TThresholdAnalysis : public TScanAnalysis {
   
- private:
-  
-  const float m_electronPerDac = 10;
+ private:  
+  const float m_electronPerDac = 10; //[e/DAC]
   const std::string m_analisysName = "TThresholdAnalysis";
   
   int m_startPulseAmplitude;
@@ -51,27 +137,18 @@ class TThresholdAnalysis : public TScanAnalysis {
   
   double m_cutChi2; // Or float ?
   
-  struct tm* m_time;
+  std::map<int,TThresholdResultChip> m_resultChip;
+  std::map<int,common::TStatVar> m_threshold;
+  std::map<int,common::TStatVar> m_noise;
   
-  FILE* m_filePixelNoHits;
-  FILE* m_filePixelStuck;
-  FILE* m_filePixelNoThreshold;
-  FILE* m_fileRawData;
-  FILE* m_fileFitResults;
-  
-  // TODO
-  //std::vector<FILE*> m_filePixelNoHits;
-  //std::vector<FILE*> m_filePixelStuck;
-  //std::vector<FILE*> m_filePixelNoThreshold;
-  //std::vector<FILE*> m_fileRawData;
-  
-  std::string GetFileName(TChipIndex aChipIndex,std::string fileType);
-  bool   CheckPixelNoHits(TGraph* aGraph);
-  bool   CheckPixelStuck (TGraph* aGraph);
-  std::pair<double,double>   DoFit           (TGraph* aGraph);
+  std::string GetFileName(common::TChipIndex aChipIndex,
+			  std::string fileType);
+  bool                      CheckPixelNoHits(TGraph* aGraph);
+  bool                      CheckPixelStuck (TGraph* aGraph);
+  common::TErrFuncFitResult DoFit           (TGraph* aGraph);
   
   bool HasData(TScanHisto &scanHisto, 
-	       TChipIndex idx, 
+	       common::TChipIndex idx, 
 	       int col);
   
  protected:
