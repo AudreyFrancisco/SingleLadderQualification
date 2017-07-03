bf5dca4043839a08fbb6407aa54eaeedf8bf023a
Antonio Franco <antonio.franco@cern.ch>
2017-07-03 17:20:13 +0200

    the test of create activity

--- DataBaseSrc/main.cpp
+++ DataBaseSrc/main.cpp
@@ -80,6 +80,51 @@ int main()
 	cout << theCompTable->Print(&oneComponent) << endl;
 
 
+	// --- create activity test
+	ActivityDB *theActivityTable = new ActivityDB(theDB);
+	ActivityDB::activity Attiv;
+	ActivityDB::member Mem;
+	ActivityDB::parameter Par;
+	ActivityDB::attach Att;
+
+	tm ts; ts.tm_year = 1963; ts.tm_mon = 4; ts.tm_mday = 18;
+
+	Attiv.Location = 1;
+	Attiv.EndDate = time(NULL);
+	Attiv.Lot = "TestAntonio";
+	Attiv.Name = "Test_db";
+	Attiv.Position = "Position1";
+	Attiv.Result = 1;
+	Attiv.StartDate = mktime(&ts);
+	Attiv.Status = 1;
+	Attiv.Type = 1;
+	Attiv.User = 1;
+
+	for(int i=0; i < 3; i++){
+		Mem.Leader = 1 +i;
+		Mem.ProjectMember = 1+i;
+		Mem.User = 1+i;
+		Attiv.Members.push_back(Mem);
+	}
+
+	for(int i=0; i < 3; i++){
+		Par.ActivityParameter = 1 +i;
+		Par.User = 1+i;
+		Par.Value = 9.5+i;
+		Attiv.Parameters.push_back(Par);
+	}
+
+	for(int i=0; i < 3; i++){
+		Att.Category = 1+i;
+		Att.LocalFileName = "AttachTest" + i + ".txt";
+		Att.User = i+1;
+		Att.RemoteFileName = "Att" + i + ".txt";
+		Attiv.Attachments.push_back(Att);
+	}
+
+	theActivityTable->Create(&Attiv);
+	cout << theActivityTable->DumpResponse() << endl;
+
 }
 
 
