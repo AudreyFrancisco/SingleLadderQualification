ece9dd10ea4cd3ee82e84663340c9e91e547e04e
Dimitra Andreou <dimitra.andreou@cern.ch>
2017-07-06 18:34:01 +0200

    combo details

--- GUI/scanthread.h
+++ GUI/scanthread.h
@@ -1,11 +1,22 @@
 #ifndef SCANTHREAD_H
 #define SCANTHREAD_H
-
-
-class ScanThread
+#include <QThread>
+#include <QtCore>
+class ScanThread:public QObject
 {
+    Q_OBJECT
+
+
+
+signals:
+    void resultReady();
+
+public slots:
+    void process();
 public:
     ScanThread();
+    //void run();
+
 };
 
 #endif // SCANTHREAD_H
