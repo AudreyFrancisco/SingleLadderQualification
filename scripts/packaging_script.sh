358b819716c8211f823fcd6e933d554c7bc70d11
Felix Reidt <felix.reidt@cern.ch>
2017-08-02 13:36:33 +0200

    removing an existing software package before creating the new archive

--- scripts/packaging_script.sh
+++ scripts/packaging_script.sh
@@ -8,6 +8,7 @@ git status > git_status.txt 2>&1
 git log -1 > git_status.txt 2>&1
 git diff   > git_status.txt 2>&1
 
+rm -v ../new-alpide-software.zip
 zip ../new-alpide-software.zip -r . \
     -x \*.o -x \*.so -x test_\* -x startclk -x stopclk -x .\* \
     -x analysis\* -x GUI\* -x .git\* -x .gitignore \
