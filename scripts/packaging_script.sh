ce2e82a1c8c3cabd3c45d38ec651ef79ab18e72c
Felix Reidt <felix.reidt@cern.ch>
2017-08-02 13:31:08 +0200

    working on the packaging script for the test beam

--- scripts/packaging_script.sh
+++ scripts/packaging_script.sh
@@ -8,7 +8,7 @@ git status > git_status.txt 2>&1
 git log -1 > git_status.txt 2>&1
 git diff   > git_status.txt 2>&1
 
-zip ../new-alpide-software.zip -r $(git rev-parse --show-toplevel) \
+zip ../new-alpide-software.zip -r . \
     -x \*.o -x \*.so -x test_\* -x startclk -x stopclk \
     -x analysis/\* -x GUI/\* -x .git\/* -x .gitignore \
     -x DataBaseSrc/\* -x exe/\*
