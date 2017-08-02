c03f43a320051a82664552dba5f112fa4ebf7e36
Felix Reidt <felix.reidt@cern.ch>
2017-08-02 13:30:32 +0200

    working on the packaging script for the test beam

--- scripts/packaging_script.sh
+++ scripts/packaging_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd $(git rev-parse --show-toplevel)
+cd "$(git rev-parse --show-toplevel)"
 
 git clean -x -f -d
 git checkout .
@@ -8,5 +8,7 @@ git status > git_status.txt 2>&1
 git log -1 > git_status.txt 2>&1
 git diff   > git_status.txt 2>&1
 
-cd ../
-zip new-alpide-software.zip -r $(git rev-parse --show-toplevel) -x \*.o -x \*.so -x test_\* -x startclk -x stopclk
+zip ../new-alpide-software.zip -r $(git rev-parse --show-toplevel) \
+    -x \*.o -x \*.so -x test_\* -x startclk -x stopclk \
+    -x analysis/\* -x GUI/\* -x .git\/* -x .gitignore \
+    -x DataBaseSrc/\* -x exe/\*
