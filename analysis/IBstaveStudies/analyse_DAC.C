c1549cad5e0ba92a8add45e0057887f8637823e0
Felix Reidt <felix.reidt@cern.ch>
2017-10-12 11:08:03 +0200

    - updated draw strings and fitting parameters - DAC analysis   * 200 DAC as fix point   * 1.3V to 2.0V - run_DAC_analysis.sh   * re-execution compatibility   * pre-process TEMP measurement files as well

--- analysis/IBstaveStudies/analyse_DAC.C
+++ analysis/IBstaveStudies/analyse_DAC.C
@@ -18,16 +18,16 @@ void analyse_DAC(TString inputFile, TString DAC, Int_t chip = -1) {
   //std::cout << DAC << std::endl;
 
   TString draw_str = "Voltage:MeasValue";
-  TString cut_str  = "(SetValue==160)";
+  TString cut_str  = "(SetValue==200)";
   cut_str += TString::Format("&&(DAC==\"%s\")", DAC.Data());
 
-  float expected_value_base = 160./255.;
+  float expected_value_base = 200./255.;
 
   std::cout << std::setprecision(3);
 
   std::cout << "#Voltage\tExpecteValue\tMeasMean\tMeasRMS\tDevMean\tDevMean (%)\tMeasured Voltage\tMeasured Deviation" << std::endl;
 
-  for (float voltage=1.62; voltage<2.00; voltage+=0.02) {
+  for (float voltage=1.30; voltage<2.00; voltage+=0.02) {
     float mean = 0.;
     float rms  = 0.;
     float expected_value = expected_value_base * voltage;
@@ -36,6 +36,7 @@ void analyse_DAC(TString inputFile, TString DAC, Int_t chip = -1) {
       cut_str_mod += TString::Format("&&(Chip==%d)", i_chip);
       cut_str_mod += TString::Format("&&(Voltage>%0.3f)&&(Voltage<%0.3f)", voltage-0.001, voltage+0.001);
       ULong64_t n = t->Draw(draw_str.Data(), cut_str_mod.Data(), "goff");
+      if (n==0) continue;
       float meas_value = t->GetV2()[0];
       float dev        = expected_value - meas_value;
       //std::cout << voltage << "\t" << i_chip << "\t" << expected_value << "\t" << meas_value << "\t" << dev << "\t" << dev/expected_value*100. << std::endl;
@@ -44,8 +45,8 @@ void analyse_DAC(TString inputFile, TString DAC, Int_t chip = -1) {
    }
    rms = TMath::Sqrt(rms-mean*mean);
    float measured_voltage = mean/expected_value_base;
-   std::cout << voltage << "\t" << expected_value << "\t" << mean << "\t" << rms 
-             << "\t" << expected_value-mean << "\t" << (expected_value-mean)/expected_value*100. << "\t" 
+   std::cout << voltage << "\t" << expected_value << "\t" << mean << "\t" << rms
+             << "\t" << expected_value-mean << "\t" << (expected_value-mean)/expected_value*100. << "\t"
    << measured_voltage << "\t" << measured_voltage - voltage << std::endl;
  }
 }
--- analysis/IBstaveStudies/run_DAC_analysis.sh
+++ analysis/IBstaveStudies/run_DAC_analysis.sh
@@ -17,23 +17,28 @@ command -v root >/dev/null 2>&1 || { echo "CERN ROOT not found.  Aborting." >&2;
 #################################################################
 cd ${INPUT_FOLDER}
 
+
 OUTFILE=DAC_summary_${SUFFIX}.dat
-echo -e "# DAC\tChip\tV\tDAC value\tADC value" > ${OUTFILE}
 
-## generate a summary file (ASCII table)
-for f in $(ls *DAC_*_${SUFFIX}.dat | grep -v summary )
-do
-    DAC=$(echo $f   | cut -d'_' -f2 )
-    CHIP=$(echo $f  | cut -d'_' -f3 )
-    CHIP=${CHIP:4}
-    VOLT=$(echo $f  | cut -d'_' -f5 )
-    VOLT=${VOLT::-1}
+if [ ! -f $OUTFILE ]
+then
+    echo -e "# DAC\tChip\tV\tDAC value\tADC value" > ${OUTFILE}
 
-    while IFS= read LINE
+    ## generate a summary file (ASCII table)
+    for f in $(ls *DAC_*_${SUFFIX}.dat | grep -v summary )
     do
-	echo "${DAC} ${CHIP} ${VOLT} ${LINE}" >> ${OUTFILE}
-    done < "$f"
-done
+        DAC=$(echo $f   | cut -d'_' -f2 )
+        CHIP=$(echo $f  | cut -d'_' -f3 )
+        CHIP=${CHIP:4}
+        VOLT=$(echo $f  | cut -d'_' -f5 )
+        VOLT=${VOLT::-1}
+
+        while IFS= read LINE
+        do
+	        echo "${DAC} ${CHIP} ${VOLT} ${LINE}" >> ${OUTFILE}
+        done < "$f"
+    done
+fi
 
 DACS=$(cat ${OUTFILE}       | grep -v "\#" | cut -f1 -d' ' | uniq       | tr '\n' ' ' )
 CHIPS=$(cat ${OUTFILE}      | grep -v "\#" | cut -f2 -d' ' | sort -u -n | tr '\n' ' ' )
@@ -50,8 +55,10 @@ echo
 echo "SET VALUES: "${DAC_VALUES}
 echo
 
-## convert ASCII table to a ROOT TTree
-root -l -b <<EOF
+if [ ! -f DAC_summary_${SUFFIX}.root ]
+then
+   ## convert ASCII table to a ROOT TTree
+   root -l -b <<EOF
 TFile* f = new TFile("DAC_summary_${SUFFIX}.root", "RECREATE");
 TTree*  t = new TTree("DACsummary","");
 Long64_t nlines = t->ReadFile("${OUTFILE}", "DAC/C:Chip/s:Voltage/D:SetValue/b:MeasValue/D");
@@ -62,8 +69,9 @@ delete f;
 f = 0x0;
 EOF
 
-## archive the dat files
-tar cjf DAC_summary_${SUFFIX}_archive.tar.bz2 *DAC_*_${SUFFIX}.dat --remove-files
+    ## archive the dat files
+    tar cjf DAC_summary_${SUFFIX}_archive.tar.bz2 *DAC_*_${SUFFIX}.dat --remove-files --exclude=DAC_summary${SUFFIX}.dat
+fi
 
 ## analyse the data
 for d in VCASN VCASP VCASN2 VCLIP VTEMP
@@ -75,8 +83,10 @@ done
 
 
 ## AVDD measurement file
-cat AVDD_*_${SUFFIX}.dat > AVDD_summary_${SUFFIX}.dat
-## convert ASCII table to a ROOT TTree
+if [ ! -f AVDD_summary_${SUFFIX}.root ]
+then
+    cat AVDD_*_${SUFFIX}.dat > AVDD_summary_${SUFFIX}.dat
+    ## convert ASCII table to a ROOT TTree
 root -l -b <<EOF
 TFile* f = new TFile("AVDD_summary_${SUFFIX}.root", "RECREATE");
 TTree*  t = new TTree("AVDDsummary","");
@@ -88,8 +98,30 @@ delete f;
 f = 0x0;
 EOF
 
-## archive the dat files
-tar cjf AVDD_summary_${SUFFIX}_archive.tar.bz2 AVDD_*_${SUFFIX}.dat --remove-files
+    ## archive the dat files
+    tar cjf AVDD_summary_${SUFFIX}_archive.tar.bz2 AVDD_*_${SUFFIX}.dat --remove-files --exclude=AVDD_summary${SUFFIX}.dat
+fi
 root -l -b -q ${SCRIPT_DIR}/analyse_AVDD.C'("'$(readlink -f AVDD_summary_${SUFFIX}.root)'")'
+
+
+## TEMP measuremnet
+if [ ! -f TEMP_summary_${SUFFIX}.root ]
+then
+    cat TEMP_*_${SUFFIX}.dat > TEMP_summary_${SUFFIX}.dat
+   ## convert ASCII table to a ROOT TTree
+   root -l -b <<EOF
+TFile* f = new TFile("TEMP_summary_${SUFFIX}.root", "RECREATE");
+TTree*  t = new TTree("TEMPsummary","");
+Long64_t nlines = t->ReadFile("${OUTFILE}", "Sample/s:Temperature/D");
+std::cout << "Read " << nlines << " lines into the TTree" << std::endl;
+t->Write();
+f->Close();
+delete f;
+f = 0x0;
+EOF
+
+    ## archive the dat files
+    tar cjf TEMP_summary_${SUFFIX}_archive.tar.bz2 TEMP_*_${SUFFIX}.dat --remove-files --exclude=TEMP_summary${SUFFIX}.dat
+fi
 
 echo "# done."
