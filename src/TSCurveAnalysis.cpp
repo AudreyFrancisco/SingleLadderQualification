9492a704b770b5d5853fc3d374d6806019c1781a
Markus Keil <markus.keil@cern.ch>
2017-11-08 09:18:29 +0100

    TSCurveAnalysis: bugfix

--- src/TSCurveAnalysis.cpp
+++ src/TSCurveAnalysis.cpp
@@ -265,8 +265,9 @@ void TSCurveAnalysis::Finalize()
 
   for (unsigned int ihic = 0; ihic < m_hics.size(); ihic++) {
     TSCurveResultHic *hicResult = (TSCurveResultHic*) m_result->GetHicResults().at(m_hics.at(ihic)->GetDbId());
-    for (unsigned int ichip = 0; ichip < hicResult->m_chipResults.size(); ichip ++) {
-      TSCurveResultChip *chipResult = (TSCurveResultChip*) hicResult->m_chipResults.at(ichip);
+    for (unsigned int ichip = 0; ichip < m_chipList.size(); ichip ++) {
+      if (! (m_hics.at(ihic)->ContainsChip(m_chipList.at(ichip)))) continue;
+      TSCurveResultChip *chipResult = (TSCurveResultChip*) m_result->GetChipResult(m_chipList.at(ichip));
       if (chipResult->m_thresholdAv < hicResult->m_minChipAv) hicResult->m_minChipAv = chipResult->m_thresholdAv;
       if (chipResult->m_thresholdAv > hicResult->m_maxChipAv) hicResult->m_maxChipAv = chipResult->m_thresholdAv;   
     }
