28d50b3b26b43fab5b05a519196225b7718a7d60
Felix Reidt <felix.reidt@cern.ch>
2018-01-17 09:37:45 +0100

    fixing formatting

--- inc/TFifoTest.h
+++ inc/TFifoTest.h
@@ -17,12 +17,12 @@ private:
   TAlpide *m_testChip;
   int m_boardIndex;
 
-  int   GetChipById(std::vector<TAlpide *> chips, int previousId);
-  void  ReadMem    (TAlpide *chip, int ARegion, int AOffset, int &AValue, bool &exception);
-  void  WriteMem   (TAlpide *chip, int ARegion, int AOffset, int AValue);
-  bool  TestPattern(int pattern, bool &exception);
+  int GetChipById(std::vector<TAlpide *> chips, int previousId);
+  void ReadMem(TAlpide *chip, int ARegion, int AOffset, int &AValue, bool &exception);
+  void WriteMem(TAlpide *chip, int ARegion, int AOffset, int AValue);
+  bool TestPattern(int pattern, bool &exception);
   //  float m_voltageScale;
-  //int   m_mlvdsStrength;
+  // int   m_mlvdsStrength;
 
 protected:
   THisto CreateHisto();
@@ -32,25 +32,26 @@ public:
             std::vector<TReadoutBoard *> boards, std::deque<TScanHisto> *histoque,
             std::mutex *aMutex);
   ~TFifoTest() {};
-  void Init      ();
-  void Execute   ();
-  void Terminate ();
-  void LoopEnd   (int loopIndex);
-  void LoopStart (int loopIndex) {
+  void Init();
+  void Execute();
+  void Terminate();
+  void LoopEnd(int loopIndex);
+  void LoopStart(int loopIndex) {
     m_value[loopIndex] = m_start[loopIndex];
   };
   void PrepareStep(int loopIndex);
   bool IsNominal() {
-    return ((((TFifoParameters*)m_parameters)->voltageScale > 0.99) && (((TFifoParameters*)m_parameters)->voltageScale < 1.01));
+    return ((((TFifoParameters *)m_parameters)->voltageScale > 0.99) &&
+            (((TFifoParameters *)m_parameters)->voltageScale < 1.01));
   };
   bool IsLower() {
-    return (((TFifoParameters*)m_parameters)->voltageScale < 0.9);
+    return (((TFifoParameters *)m_parameters)->voltageScale < 0.9);
   };
   bool IsUpper() {
-    return (((TFifoParameters*)m_parameters)->voltageScale > 1.1);
+    return (((TFifoParameters *)m_parameters)->voltageScale > 1.1);
   };
   int GetDriver() {
-    return ((TFifoParameters*)m_parameters)->mlvdsStrength;
+    return ((TFifoParameters *)m_parameters)->mlvdsStrength;
   };
 };
 
