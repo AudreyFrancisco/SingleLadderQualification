52a188b6da966ef374f806d9f2d509343835f032
Felix Reidt <felix.reidt@cern.ch>
2018-01-29 09:39:11 +0100

    bug fixes in the formatting tools

--- .format-check.sh
+++ .format-check.sh
@@ -20,16 +20,16 @@ IGNORE_STRING=$(join \| "${IGNORE_SET[@]}")
 
 SOURCES=$(find . | egrep -v ${IGNORE_STRING} | egrep "\.h$|\.hh$|\.c$|\.cc$|\.C$|\.cpp$")
 
-os_version=$(lsb_release -d | grep "CentOS Linux release 7" | wc -l 2> /dev/null)
+os_version=$(lsb_release -d 2> /dev/null | grep "CentOS Linux release 7" | wc -l 2> /dev/null)
 if [[ "$os_version" -ne 1 ]]
 then
-    "automatic formatting only available on CentOS Linux release 7"
+    echo "automatic formatting only available on CentOS Linux release 7"
     exit 3
 fi
 
 if [[ !"$(clang-format --version | sed -nre 's/^[^0-9]*(([0-9]+\.)*[0-9]+).*/\1/p' | tr -d '.' 2>/dev/null)" -gt 400 ]]
 then
-    "automatic formatting only available on if clang-format 4.0.0 or newer are available";
+    echo "automatic formatting only available on if clang-format 4.0.0 or newer are available";
     exit 2
 fi
 
