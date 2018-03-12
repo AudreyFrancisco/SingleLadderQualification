e96683f00c9a5fc8825b69c7f0d87cf3a9bd5342
Sergey <Serhiy.senyukov@cern.ch>
2018-03-12 09:45:52 +0100

    Implemented saving parameters

--- ImpedanceTest/IV_scan.py
+++ ImpedanceTest/IV_scan.py
@@ -3,7 +3,14 @@
 #
 # WARNING: RTS/CTS should be used, as otherwise the PSU might not process
 #          every command
-#
+# 
+
+import base64
+from Common import *
+from myDBlib import *
+
+import logging
+import os
 
 import serial
 import sys
@@ -54,7 +61,7 @@ def measureCurr(sour):
         val[c] = float(sour.readline())
     print "%0.4fA\t%0.4fA\t%0.4fA" % ( val[0], val[1], val[2]) 
     
-def doIVcurve(sour, channel, max_volt, nsteps, output_file):
+def doIVcurve(sour, channel, max_volt, nsteps, output_file, test_ok, resistances):
         
     if channel==0:
         channel_name = "DVDD"
@@ -90,11 +97,14 @@ def doIVcurve(sour, channel, max_volt, nsteps, output_file):
             break;
 
     resistance_average/=nsteps
-    print("Impedance of %s is %f" %(channel_name, resistance_average))
+    print("Impedance of %s is %f" %(channel_name, resistance_average)) 
+    resistances[channel] = resistance_average
     if resistance_average>100:
-        print("Test OK")
+        print("Test OK")  
+        test_ok = test_ok*1
     else:
-        print("Test failed")
+        print("Test failed") 
+        test_ok = test_ok*0
     sour.write("SOUR:VOLT 0.0\n")
 
 def init4030(hameg, i_max):
@@ -136,7 +146,87 @@ def init4030(hameg, i_max):
     hameg.write("SOUR:VOLT 0.0\n")
     hameg.write("SOUR:CURR %f\n" % i_max[2])
     time.sleep(0.1);
-    hameg.write("OUTP ON\n")
+    hameg.write("OUTP ON\n")      
+    
+def saveToDB(HIC_name, test_ok, resistances):
+    # read the Configuration
+    myConf = Configuration("DBConfig.cfg")
+
+    #logging setup    
+    lg = setUpTheLogger(myConf)
+    lg.info("Start to save results in DB")
+
+    #read the command line
+    #inpPar = ManageCommandLineArguments(argv)
+    #inpPar = ValidateInputParameters(inpPar)
+
+    #theActTag = inpPar.activity + " " + inpPar.component  # this is an extra info to create the activity : the name of an activity contains the ComponentId field
+    
+    # Read from the Configuration file 
+    DBUser = myConf.GetItem("DBUSER")           # The User name used to open the DB connection 
+    DBProject = myConf.GetItem('DBNAME')        # The DB Name used to open the DB connection 
+    DBLocation = myConf.GetItem('LOCATION')     # The Location that will be used for create activities
+
+    # connect to the DB
+    itsDB = DB(project=DBProject,userName=DBUser,location=DBLocation)
+    if itsDB is None:
+        lg.error("DB not open !")  
+    else:
+        lg.info("DB is open")    
+       
+    activityResult = ''
+                         
+    if test_ok:
+        activityResult='OK'
+    else:
+        activityResult='NOK'
+    
+    actResult = itsDB.CreateCompActivity(HIC_name, "OB-HIC Impedance Test", 
+                                  "Impedance test of ", activityResult, False )
+    if actResult.ErrorCode != 0:
+        lg.error("Error create the activity %s -> %s!" % ("OB-HIC Impedance Test",actResult.ErrorMessage))
+        return 1
+    else:
+        lg.info(" Activity %s open." % "OB-HIC Impedance Test")
+    actID = actResult.ID   
+    
+    parameter_name = ''
+    
+    for channel in range(3):    
+       if channel==0:
+        parameter_name = 'OB-HIC DVDD impedance'
+       elif channel==1:
+        parameter_name = 'OB-HIC AVDD impedance'
+       elif channel==2:
+        parameter_name = 'OB-HIC BIAS impedance'
+    
+       isGood, Value = itsDB.ParameterRead(actID, parameter_name)   
+    
+       if isGood:
+        # This is a change
+           actResult = itsDB.ParameterChange(actID,parameter_name, resistances[channel])
+       else: 
+        # this is a Creation
+           actResult = itsDB.ParameterWrite(actID,parameter_name, resistances[channel])
+       if not actResult:
+           lg.error("Error writing parameter %s!" % (parameter_name))
+           return 1
+       else:
+           lg.info(" Parameter %s = %f written." % (parameter_name, resistances[channel]))    
+               
+    actResult = itsDB.ActivityMemberAssign(actID)
+    if actResult.ErrorCode != 0:
+        lg.error("Error assigning member %s: %s!" % (itsDB.selectedMembers,actResult.ErrorMessage))
+        return 1
+        
+    # Finally close the activity
+    actResult = itsDB.CloseActivity(actID, activityResult)
+    if actResult.ErrorCode != 0:
+        lg.error("Error closing the activity: %s!" % (actResult.ErrorMessage))
+        return 1
+    
+    lg.info("Finished to save results in DB")
+    return 0
 
 def main():
     # set up serial port
@@ -146,21 +236,27 @@ def main():
     max_voltage = 0.2
     max_voltage_bias = 4.
     nsteps = 20 
-    nsteps_bias = 50
+    nsteps_bias = 50    
+    
+    resistances = ([0., 0., 0.])  
+    test_ok = 1
 
     sour=serial.Serial(dev, 9600, rtscts=True);
     
     init4030(sour, (current_limit, current_limit, current_limit_bias))
 
     HIC_name = raw_input("Enter HIC name: ")
-    output_file_name = 'OBHIC-' + HIC_name + '.dat'
+    HIC_name = 'OBHIC-' + HIC_name
+    output_file_name = HIC_name + '.dat'
     output_file = open(output_file_name,'w')
     
     max_voltages = ([max_voltage, max_voltage, max_voltage_bias])
     nstepss = ([nsteps, nsteps, nsteps_bias])
 
     for channel in range(3):    
-        doIVcurve(sour, channel, max_voltages[channel], nstepss[channel], output_file)
+        doIVcurve(sour, channel, max_voltages[channel], nstepss[channel], output_file, test_ok, resistances) 
+      
+    saveToDB(HIC_name, test_ok, resistances)  
     
     output_file.close()
     
