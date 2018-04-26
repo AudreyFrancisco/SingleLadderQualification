2a1491e03f41b0f351ec7d02b2626cc9df5fb86e
Matthias Bonora <matthias.bonora@cern.ch>
2018-04-26 19:30:53 +0200

    MB move Eye scan functionality in TEyeMesaurement class

--- inc/TEyeMeasurement.h
+++ inc/TEyeMeasurement.h
@@ -7,10 +7,21 @@
 #include "THisto.h"
 #include "TScan.h"
 
+class TReadoutBoardMosaic;
+
 class TEyeMeasurement : public TScan {
 private:
   TAlpide *m_testChip;
-  int      m_boardIndex;
+  int m_boardIndex;
+
+  // parameters
+  int m_max_prescale;
+  int m_min_prescale;
+  int m_max_zero_results;
+
+  // internal
+  int m_current_prescale;
+  TReadoutBoardMOSAIC *m_board;
 
 protected:
   THisto CreateHisto();
@@ -19,13 +30,16 @@ public:
   TEyeMeasurement(TScanConfig *config, std::vector<TAlpide *> chips, std::vector<THic *> hics,
                   std::vector<TReadoutBoard *> boards, std::deque<TScanHisto> *histoque,
                   std::mutex *aMutex);
-  ~TEyeMeasurement(){};
+  ~TEyeMeasurement() {};
 
   void Init();
   void Execute();
   void Terminate();
   void LoopEnd(int loopIndex);
-  void LoopStart(int loopIndex) { m_value[loopIndex] = m_start[loopIndex]; };
+  void LoopStart(int loopIndex)
+  {
+    m_value[loopIndex] = m_start[loopIndex];
+  };
   void PrepareStep(int loopIndex);
 };
 
