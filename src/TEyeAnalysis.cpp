f7d57cbdeb6b616269b3b8c57f5df9901f1d42ce
Jochen Klein <jochen.klein@cern.ch>
2018-05-14 17:15:53 +0200

    Cleaning up TEyeAnalysis

--- src/TEyeAnalysis.cpp
+++ src/TEyeAnalysis.cpp
@@ -29,16 +29,9 @@ void TEyeAnalysis::AnalyseHisto(TScanHisto *histo)
   std::cout << "in analyse histo, chipList.size = " << m_chipList.size() << std::endl;
   FILE *fp = fopen("EyeDiagram.dat", "w");
 
-  TEyeParameters *ePar = (TEyeParameters *)(m_scan->GetParameters());
-  std::ofstream   outfile_l("edge_l.dat");
-  std::ofstream   outfile_r("edge_r.dat");
-
-  std::string filename_eye = FindHicResultForChip(m_chipList.at(0))->GetOutputPath() + "/eye_D" +
-                             std::to_string(ePar->driverStrength) + "_P" +
-                             std::to_string(ePar->preemphasis) + ".pdf";
-  std::string filename_eye_root = FindHicResultForChip(m_chipList.at(0))->GetOutputPath() +
-                                  "/eye_D" + std::to_string(ePar->driverStrength) + "_P" +
-                                  std::to_string(ePar->preemphasis) + ".root";
+  std::string filename_eye = FindHicResultForChip(m_chipList.at(0))->GetOutputPath() + "/eye.pdf";
+  std::string filename_eye_root =
+      FindHicResultForChip(m_chipList.at(0))->GetOutputPath() + "/eye.root";
 
   TCanvas c;
   c.cd();
@@ -46,85 +39,74 @@ void TEyeAnalysis::AnalyseHisto(TScanHisto *histo)
 
   TFile *rootfile_eye = TFile::Open(filename_eye_root.c_str(), "RECREATE");
 
-  for (unsigned int ichip = 0; ichip < m_chipList.size(); ichip++) {
-    double step_x = histo->GetStep(m_chipList.at(ichip), 0);
-    double step_y = histo->GetStep(m_chipList.at(ichip), 1);
+  for (auto chip : m_chipList) {
+    const double step_x = histo->GetStep(chip, 0);
+    const double step_y = histo->GetStep(chip, 1);
 
-    double min_x = histo->GetMin(m_chipList.at(ichip), 0);
-    double min_y = histo->GetMin(m_chipList.at(ichip), 1);
+    const double min_x = histo->GetMin(chip, 0);
+    const double min_y = histo->GetMin(chip, 1);
 
-    const int nbin_x = histo->GetNBin(m_chipList.at(ichip), 0);
-    const int nbin_y = histo->GetNBin(m_chipList.at(ichip), 1);
+    const int nbin_x = histo->GetNBin(chip, 0);
+    const int nbin_y = histo->GetNBin(chip, 1);
 
     const int nbin_x_half = nbin_x / 2;
     const int nbin_y_half = nbin_y / 2;
     const int yband       = 2;
 
-    const std::string hname = TString::Format("h_eye_%i", m_chipList.at(ichip).chipId).Data();
-    const std::string htitle =
-        TString::Format("Eye Diagram chip %i (%s) D%i, P%i", m_chipList.at(ichip).chipId,
-                        FindHicResultForChip(m_chipList.at(ichip))->GetName().c_str(),
-                        ePar->driverStrength, ePar->preemphasis)
-            .Data();
+    const std::string hname  = TString::Format("h_eye_%i", chip.chipId).Data();
+    const std::string htitle = TString::Format("Eye Diagram chip %i (%s)", chip.chipId,
+                                               FindHicResultForChip(chip)->GetName().c_str())
+                                   .Data();
+
     TH2F h_eye(hname.c_str(), htitle.c_str(), nbin_x, min_x, min_x + nbin_x * step_x, nbin_y, min_y,
                min_y + nbin_y * step_y);
     h_eye.SetDirectory(0);
 
+    // fill histogram
     for (int xbin = 0; xbin < nbin_x; xbin++) {
       for (int ybin = 0; ybin < nbin_y; ybin++) {
-        double value = ((*histo)(m_chipList.at(ichip), xbin, ybin));
-
-        int x = min_x + xbin * step_x;
-        int y = min_y + ybin * step_y;
+        int    x     = min_x + xbin * step_x;
+        int    y     = min_y + ybin * step_y;
+        double value = (*histo)(chip, xbin, ybin);
 
         if (value != 0) {
-          std::cout << "ichip = " << ichip << ", x = " << x << ", y = " << y
-                    << ", value = " << value << " bins: " << xbin << ", " << ybin << std::endl;
+          fprintf(fp, "%d %d->%d %d->%d %e\n", chip.chipId, xbin, x, ybin, y, value);
           h_eye.SetBinContent(xbin + 1, ybin + 1, value);
-          fprintf(fp, "%d %d->%d %d->%d %e\n", ichip, xbin, x, ybin, y, value);
         }
       }
     }
 
-    std::cout << "number of bins x/y: " << nbin_x << ", " << nbin_y << std::endl;
-    h_eye.SetStats(kFALSE);
-    h_eye.Draw("colz");
-
-    double *x_left  = new double[nbin_x_half];
-    double *x_right = new double[nbin_x_half];
+    // calculate cumulative function along x (within [-yband, yband])
+    std::vector<double> x_l, x_r;
     for (int xbin = 0; xbin < nbin_x_half; ++xbin) {
-      x_left[xbin]  = (xbin != 0) ? x_left[xbin - 1] : 0.;
-      x_right[xbin] = (xbin != 0) ? x_right[xbin - 1] : 0.;
+      x_l.push_back((xbin != 0) ? x_l[xbin - 1] : 0.);
+      x_r.push_back((xbin != 0) ? x_r[xbin - 1] : 0.);
       for (int ybin = nbin_y_half - yband; ybin < nbin_y_half + yband; ++ybin) {
-        x_left[xbin] += ((*histo)(m_chipList.at(ichip), xbin, ybin));
-        x_right[xbin] += ((*histo)(m_chipList.at(ichip), nbin_x - 1 - xbin, ybin));
+        x_l.back() += (*histo)(chip, xbin, ybin);
+        x_r.back() += (*histo)(chip, nbin_x - 1 - xbin, ybin);
       }
     }
 
-    int       x_l       = -1;
-    int       x_r       = -1;
-    const int lastbin_x = std::max(0, nbin_x_half - 1);
+    // calculate opening as 85 % per-centile
+    int open_l = -1;
+    int open_r = -1;
     for (int xbin = 0; xbin < nbin_x_half; ++xbin) {
-      if ((x_l == -1) && ((x_left[xbin] / x_left[lastbin_x]) > 0.85)) x_l   = xbin;
-      if ((x_r == -1) && ((x_right[xbin] / x_right[lastbin_x]) > 0.85)) x_r = xbin;
-      printf("%2dL-%3d: %g (%g / %g)\n", ichip, xbin, x_left[xbin] / x_left[31], x_left[xbin],
-             x_left[lastbin_x]);
-      printf("%2dR-%3d: %g (%g / %g)\n", ichip, xbin, x_right[xbin] / x_right[31], x_right[xbin],
-             x_right[lastbin_x]);
-      outfile_l << ichip << " " << x_left[xbin] / x_left[lastbin_x] << std::endl;
-      outfile_r << ichip << " " << x_right[xbin] / x_right[lastbin_x] << std::endl;
+      if ((open_l == -1) && ((x_l[xbin] / x_l.back()) > 0.85)) open_l = xbin;
+      if ((open_r == -1) && ((x_r[xbin] / x_r.back()) > 0.85)) open_r = xbin;
     }
-    printf("chip %3d has eye opening left: %3d, right: %3d\n", m_chipList.at(ichip).chipId, x_l,
-           x_r);
+
+    // draw histogram
+    h_eye.SetStats(kFALSE);
+    h_eye.Draw("colz");
     TLatex l;
     l.SetTextAlign(21);
     l.SetTextFont(43);
     l.SetTextSize(16);
     l.SetNDC(kTRUE);
-    l.DrawLatex(.5, .02, TString::Format("opening: %3d - %3d", x_l, x_r));
+    l.DrawLatex(.5, .02, TString::Format("opening: %3d - %3d", open_l, open_r));
 
     // write result to files
-    std::string dirname   = FindHicResultForChip(m_chipList.at(ichip))->GetName();
+    std::string dirname   = FindHicResultForChip(chip)->GetName();
     TDirectory *rootdir   = rootfile_eye->GetDirectory(dirname.c_str());
     if (!rootdir) rootdir = rootfile_eye->mkdir(dirname.c_str());
     if (rootdir) rootdir->WriteTObject(&h_eye);
@@ -133,9 +115,6 @@ void TEyeAnalysis::AnalyseHisto(TScanHisto *histo)
     c.SetLogz(kTRUE);
     h_eye.SetMinimum(1.e-7);
     c.Print(filename_eye.c_str());
-
-    delete[] x_left;
-    delete[] x_right;
   }
   c.Print((filename_eye + "]").c_str());
   fclose(fp);
