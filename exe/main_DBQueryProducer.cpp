baa221e6fd8444713e24112e6fd1774ca8972658
Antonio Franco <antonio.franco@cern.ch>
2018-05-24 14:45:59 +0200

    Fix some things in the examples mail

--- exe/main_DBQueryProducer.cpp
+++ exe/main_DBQueryProducer.cpp
@@ -1,3 +1,41 @@
+/*
+ * \file main_DBQueryProducer.cpp
+ * \author A.Franco
+ * \date 16/Maggio/2018
+ *
+ * Copyright (C) 2018
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ *
+ * ====================================================
+ *     __  __   __  _____  __   __
+ *    / / /  | / / / ___/ /  | / / SEZIONE di BARI
+ *   / / / | |/ / / /_   / | |/ /
+ *  / / / /| / / / __/  / /| / /
+ * /_/ /_/ |__/ /_/    /_/ |__/
+ *
+ * ====================================================
+ *
+ *  Description : Simple test program able to create JSON format
+ *                Activity Creation files
+ *
+ *  HISTORY
+ *
+ *
+ */
 #include "DBQueryQue.h"
 #include "DBHelpers.h"
 
@@ -6,38 +44,40 @@
 #include <thread>
 
 
-
-
 int main()
 {
-	AlpideDB *fDB;
-	bool fDatabasetype = true;
+    AlpideDB *fDB;
+    bool fDatabasetype = true;
     fDB = new AlpideDB(fDatabasetype);
     ActivityDB *myactivity = new ActivityDB(fDB);
     ActivityDB::activity activ;
 
     myactivity->theAsyncronuosQueue->SetLocalCopyEnabled(true);
+    myactivity->theAsyncronuosQueue->SetEstension(".dbq");
+    myactivity->theAsyncronuosQueue->SetLocalCopyPath("/tmp/loc");
+    myactivity->theAsyncronuosQueue->SetBasePath("/tmp");
+    myactivity->theAsyncronuosQueue->SetSpecificPath("GUIlocalQueryBackup");
 
-	        activ.Type      = 881; //fIdofactivitytype;
-	        activ.Location  = 161; //fIdoflocationtype;
-	        activ.User      = 1126; //fIdofoperator;
-	        activ.StartDate = time(0) ;//date.currentDateTime().toTime_t();
-	        activ.EndDate   = time(0); //date.currentDateTime().toTime_t();
-	        activ.Lot       = " ";
-          activ.Name = "Test of asyncronous act creation";//CreateActivityName(fHICs.at(i)->GetDbId(), fConfig->GetScanConfig());
-	        activ.Position = " ";
-	        activ.Result =
-	            -999; // apparently has to stay open here, otherwise activity is considered closed
-	        activ.Status = 83;// DbGetStatusId(fDB, fIdofactivitytype, "OPEN");
-	            DbAddParameter(fDB, activ, "Number of Working Chips", 10.0,
-	                          "");
-	            DbAddParameter(fDB, activ, "IDDD", 10.1,"");
-	        DbAddAttachment(fDB, activ, attachLog, string("/tmp/Comment.txt"), string("Peppo"));
-	        DbAddMember(fDB, activ, 1126);
-	        DbAddUri(fDB, activ, "URI test 1" , "http://www.cern.ch");
-	        DbAddInComp(fDB, activ, 11956 , 1221);
-			DbAddOutComp(fDB, activ, 11956 , 1241);
-	        activ.Result = 822; //DbGetResultId(fDB, fIdofactivitytype, fHICs.at(i)->GetClassification());
-	        myactivity->CreateAsyncronous(&activ);
+    activ.Type      = 881; //fIdofactivitytype;
+    activ.Location  = 161; //fIdoflocationtype;
+    activ.User      = 1126; //fIdofoperator;
+    activ.StartDate = time(0) ;//date.currentDateTime().toTime_t();
+    activ.EndDate   = time(0); //date.currentDateTime().toTime_t();
+    activ.Lot       = " ";
+    activ.Name = "Test of asyncronous act creation";//CreateActivityName(fHICs.at(i)->GetDbId(), fConfig->GetScanConfig());
+    activ.Position = " ";
+    activ.Result =
+       -999; // apparently has to stay open here, otherwise activity is considered closed
+    activ.Status = 83;// DbGetStatusId(fDB, fIdofactivitytype, "OPEN");
+    DbAddParameter(fDB, activ, "Number of Working Chips", 10.0,"");
+    DbAddParameter(fDB, activ, "IDDD", 10.1,"");
+    DbAddAttachment(fDB, activ, attachLog, string("/tmp/Comment.txt"), string("Peppo"));
+    DbAddMember(fDB, activ, 1126);
+    DbAddUri(fDB, activ, "URI test 1" , "http://www.cern.ch");
+    DbAddInComp(fDB, activ, 11956 , 1221);
+    DbAddOutComp(fDB, activ, 11956 , 1241);
+    activ.Result = 822; //DbGetResultId(fDB, fIdofactivitytype, fHICs.at(i)->GetClassification());
+    myactivity->CreateAsyncronous(&activ);
 
+    return(0);
 }
--- exe/main_DBQueueConsumer.cpp
+++ exe/main_DBQueueConsumer.cpp
@@ -1,3 +1,42 @@
+/*
+ * \file main_DBQueueConsumer.cpp
+ * \author A.Franco
+ * \date 16/Maggio/2018
+ *
+ * Copyright (C) 2018
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ *
+ * ====================================================
+ *     __  __   __  _____  __   __
+ *    / / /  | / / / ___/ /  | / / SEZIONE di BARI
+ *   / / / | |/ / / /_   / | |/ /
+ *  / / / /| / / / __/  / /| / /
+ * /_/ /_/ |__/ /_/    /_/ |__/
+ *
+ * ====================================================
+ *
+ *  Description : An Activity Creation program that reads the
+ *                activity structure from JSON format files
+ *                and calls the DB API
+ *
+ *  HISTORY
+ *
+ *
+ */
 #include "DBQueryQue.h"
 
 #include <unistd.h>
@@ -7,55 +46,62 @@
 
 int main()
 {
-	bool fDatabasetype = true;
-	DBQueueConsumer* theQueue = new DBQueueConsumer(fDatabasetype);
-	// Configuration
-	theQueue->cfgExtension = ".dbq";
-	theQueue->cfgBasePath = "/tmp";
-	theQueue->cfgSpecificPath = "NewAlpide";
-	theQueue->cfgStatusFile = "/tmp/DBQueueConsumerState.dat";
-	theQueue->cfgMaxReconAtt = 9999;
-	
-	// run the thread
-	thread engine(&DBQueueConsumer::Run, theQueue);
-	vector<string> list;
-	char ch;
-	do {
-	  cout<<"\nDBQ>";
-	  ch = getchar();
-	  switch(ch) {
-	  case 'c':
-		  cout << "** DBQueueConsumer Configuration **" << endl;
-		  cout << " DataBaseType :" << (!theQueue->bDatabasetype ?"PRODUCTION" : "TEST") << endl;
-		   cout << " Running on the Project : " << theQueue->GetProjectId() << endl;
-		  cout << " Status filename :" << theQueue->cfgStatusFile << endl;
-		  cout << " Queue file extension :" << theQueue->cfgExtension << endl;
-		  cout << " Queue files base path :" << theQueue->cfgBasePath << endl;
-		  cout << " Queue files specific path :" << theQueue->cfgSpecificPath << endl;
-		  cout << " Maximum reconnection attempts :" << theQueue->cfgMaxReconAtt << endl;
-		  break;
-	  case 's':
-		  cout << "** DBQueueConsumer Status **" << endl;
-		  cout << " File :" << theQueue->theFileName << endl;
-		  cout << " State :" << theQueue->theState << endl;
-		  cout << " The Queue has " << theQueue->GetQueueLenght() << " elements" << endl;
-		  break;
-	  case 'q':
-		  cout << "** DBQueueConsumer Queue **" << endl;
-		  list = theQueue->GetQueue();
-		  for(int i=0;i<list.size();i++)
-			  cout << list.at(i) << endl;
-		  break;
-	  case 'h':
-	  case '?':
-		  cout << "** DBQueueConsumer Help **" << endl;
-		  cout << "c := print the configuration" << endl;
-		  cout << "s := print the status" << endl;
-		  cout << "q := dump the list of queued files" << endl;
-		  cout << "e := exit" << endl;
-	  }
-	}
-	while(ch!='e');
-	cout <<  "Bye bye !" << endl;
+    bool fDatabasetype = true;
+    DBQueueConsumer* theQueue = new DBQueueConsumer(fDatabasetype);
 
+    // Configuration
+    theQueue->cfgExtension = ".dbq";
+    theQueue->cfgBasePath = "/tmp";
+    theQueue->cfgSpecificPath = "NewAlpide";
+    theQueue->cfgStatusFile = "/tmp/DBQueueConsumerState.dat";
+    theQueue->cfgMaxReconAtt = 9999;
+
+    // Run the thread engine that consumes the queue
+    thread engine(&DBQueueConsumer::Run, theQueue);
+
+    // Start the console
+    vector<string> list;
+    char ch;
+    do {
+        cout<<"\nDBQ>";
+        ch = getchar();
+        switch(ch) {
+        case 'c':
+            cout << "** DBQueueConsumer Configuration **" << endl;
+            cout << " DataBaseType :" << (!theQueue->bDatabasetype ?"PRODUCTION" : "TEST") << endl;
+            cout << " Running on the Project : " << theQueue->GetProjectId() << endl;
+            cout << " Status filename :" << theQueue->cfgStatusFile << endl;
+            cout << " Queue file extension :" << theQueue->cfgExtension << endl;
+            cout << " Queue files base path :" << theQueue->cfgBasePath << endl;
+            cout << " Queue files specific path :" << theQueue->cfgSpecificPath << endl;
+            cout << " Maximum reconnection attempts :" << theQueue->cfgMaxReconAtt << endl;
+            break;
+        case 's':
+            cout << "** DBQueueConsumer Status **" << endl;
+            cout << " File :" << theQueue->theFileName << endl;
+            cout << " State :" << theQueue->theState << endl;
+            cout << " The Queue has " << theQueue->GetQueueLenght() << " elements" << endl;
+            break;
+        case 'q':
+            cout << "** DBQueueConsumer Queue **" << endl;
+            list = theQueue->GetQueue();
+            for(int i=0;i<list.size();i++)
+                cout << list.at(i) << endl;
+            break;
+        case 'h':
+        	case '?':
+            cout << "** DBQueueConsumer Help **" << endl;
+            cout << "c := print the configuration" << endl;
+            cout << "s := print the status" << endl;
+            cout << "q := dump the list of queued files" << endl;
+            cout << "e := exit" << endl;
+        }
+    }
+    while(ch!='e');
+
+    theQueue->executeTheRun = false
+    engine.join();
+
+    cout <<  "Bye bye !" << endl;
+    return(0);
 }
