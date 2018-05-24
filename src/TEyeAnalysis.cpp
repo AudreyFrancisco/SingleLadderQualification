3e4337bf7ea814593e1689cbe81d11460ce9cfbf
Jochen Klein <jochen.klein@cern.ch>
2018-05-24 20:39:26 +0200

    Improve analysis and plotting of eye diagrams

--- src/TEyeAnalysis.cpp
+++ src/TEyeAnalysis.cpp
@@ -1,4 +1,5 @@
 #include "TEyeAnalysis.h"
+#include "TArrow.h"
 #include "TCanvas.h"
 #include "TEyeMeasurement.h"
 #include "TFile.h"
@@ -52,12 +53,14 @@ void TEyeAnalysis::AnalyseHisto(TScanHisto *histo)
     const double min_x = histo->GetMin(chip, 0);
     const double min_y = histo->GetMin(chip, 1);
 
-    const int nbin_x = histo->GetNBin(chip, 0);
-    const int nbin_y = histo->GetNBin(chip, 1);
+    const int nbin_x = histo->GetNBin(chip, 0) - 1;
+    const int nbin_y = histo->GetNBin(chip, 1) - 1;
 
-    const int nbin_x_half = nbin_x / 2;
-    const int nbin_y_half = nbin_y / 2;
-    const int yband       = 2;
+    const int    nbin_x_half = nbin_x / 2;
+    const int    nbin_y_half = nbin_y / 2;
+    const int    xband       = 2;
+    const int    yband       = 2;
+    const double percent     = 0.98;
 
     TEyeResultHic *hicResult = (TEyeResultHic *)FindHicResultForChip(chip);
     Int_t driverStrength     = ((TEyeParameters *)hicResult->GetScanParameters())->driverStrength;
@@ -70,8 +73,9 @@ void TEyeAnalysis::AnalyseHisto(TScanHisto *histo)
                         hicResult->GetName().c_str(), driverStrength, preemphasis)
             .Data();
 
-    TH2F h_eye(hname.c_str(), htitle.c_str(), nbin_x, min_x, min_x + nbin_x * step_x, nbin_y, min_y,
-               min_y + nbin_y * step_y);
+    TH2F h_eye(hname.c_str(), htitle.c_str(), nbin_x, min_x - .5 * step_x,
+               min_x - .5 * step_x + nbin_x * step_x, nbin_y, min_y - .5 * step_y,
+               min_y - .5 * step_y + nbin_y * step_y);
     h_eye.SetDirectory(0);
 
     // fill histogram
@@ -99,34 +103,68 @@ void TEyeAnalysis::AnalyseHisto(TScanHisto *histo)
       }
     }
 
+    // calculate cumulative function along y (within [-xband, xband])
+    std::vector<double> y_l, y_r;
+    for (int ybin = 0; ybin < nbin_y_half; ++ybin) {
+      y_l.push_back((ybin != 0) ? y_l[ybin - 1] : 0.);
+      y_r.push_back((ybin != 0) ? y_r[ybin - 1] : 0.);
+      for (int xbin = nbin_x_half - xband; xbin < nbin_x_half + xband; ++xbin) {
+        y_l.back() += (*histo)(chip, xbin, ybin);
+        y_r.back() += (*histo)(chip, xbin, nbin_y - 1 - ybin);
+      }
+    }
+
     // calculate opening as 85 % per-centile
     int open_l = -1;
     int open_r = -1;
     for (int xbin = 0; xbin < nbin_x_half; ++xbin) {
-      if ((open_l == -1) && ((x_l[xbin] / x_l.back()) > 0.85)) open_l = xbin;
-      if ((open_r == -1) && ((x_r[xbin] / x_r.back()) > 0.85)) open_r = xbin;
+      if ((open_l == -1) && ((x_l[xbin] / x_l.back()) > percent)) open_l = xbin;
+      if ((open_r == -1) && ((x_r[xbin] / x_r.back()) > percent)) open_r = xbin;
+    }
+
+    int open_u = -1;
+    int open_b = -1;
+    for (int ybin = 0; ybin < nbin_y_half; ++ybin) {
+      if ((open_b == -1) && ((y_l[ybin] / y_l.back()) > percent)) open_b = ybin;
+      if ((open_u == -1) && ((y_r[ybin] / y_r.back()) > percent)) open_u = ybin;
     }
 
     // draw histogram
     h_eye.SetStats(kFALSE);
     h_eye.Draw("colz");
+    c.Update();
     TLatex l;
     l.SetTextAlign(21);
     l.SetTextFont(43);
     l.SetTextSize(16);
     l.SetNDC(kTRUE);
-    l.DrawLatex(.5, .02, TString::Format("opening: %3d - %3d", open_l, open_r));
+    double open_x_l = min_x + open_l * step_x;
+    double open_x_u = min_x + (nbin_x - 1 - open_r) * step_x;
+    double x_center = min_x + nbin_x_half * step_x;
+    double open_y_l = min_y + open_b * step_y;
+    double open_y_u = min_y + (nbin_y - 1 - open_u) * step_y;
+    double y_center = min_y + nbin_y_half * step_y;
+    TArrow arrow_x(open_x_l, y_center, open_x_u, y_center, 0.05, "<->");
+    TArrow arrow_y(x_center, open_y_l, x_center, open_y_u, 0.05, "<->");
+    if ((open_l != -1) || (open_r != -1) || (open_b != -1) || (open_u != -1)) {
+      l.DrawLatex(.5, .02, TString::Format("openings (%g) in x [%g, %g], in y [%g, %g]", percent,
+                                           open_x_l, open_x_u, open_y_l, open_y_u));
+      arrow_x.Draw();
+      arrow_y.Draw();
+    }
 
     // write result to files
     std::string dirname   = FindHicResultForChip(chip)->GetName();
     TDirectory *rootdir   = rootfile_eye->GetDirectory(dirname.c_str());
     if (!rootdir) rootdir = rootfile_eye->mkdir(dirname.c_str());
     if (rootdir) rootdir->WriteTObject(&h_eye);
-    c.SetLogz(kFALSE);
-    c.Print(filename_eye.c_str());
-    c.SetLogz(kTRUE);
-    h_eye.SetMinimum(1.e-7);
-    c.Print(filename_eye.c_str());
+    if (h_eye.GetEntries() > 0) {
+      c.SetLogz(kFALSE);
+      c.Print(filename_eye.c_str());
+      c.SetLogz(kTRUE);
+      h_eye.SetMinimum(1.e-7);
+      c.Print(filename_eye.c_str());
+    }
   }
   c.Print((filename_eye + "]").c_str());
   fclose(fp);
