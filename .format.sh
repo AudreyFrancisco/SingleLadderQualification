5a0964f59988e82d71c154c6d4fa4751f2a7b014
Felix Reidt <felix.reidt@cern.ch>
2018-06-07 13:30:00 +0200

    making formatting/format-check scripts compatible with ubuntu18.04, adding cmake target for the formatting

--- .format.sh
+++ .format.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-# change if your executable is named different
-CLANG_FORMAT=clang-format
+# use the right name for the executable
+if [[ $(which clang-format > /dev/null 2>&1; echo $?) -eq 0 ]]
+then
+    CLANG_FORMAT=$(which clang-format)
+elif [[ $(which clang-format-4.0 > /dev/null 2>&1; echo $?) -eq 0 ]]
+then
+    CLANG_FORMAT=$(which clang-format-4.0)
+fi
 
 # add all the files and directories that may not be reformatted,
 # relative to the project's root directory
@@ -9,6 +15,7 @@ IGNORE_SET=(
     analysis
     ui_
     moc_
+    qrc_
     CMakeFiles
 )
 
@@ -21,19 +28,21 @@ SOURCES=$(find . | egrep -v ${IGNORE_STRING} | egrep "\.h$|\.hh$|\.c$|\.cc$|\.C$
 
 if [ "$(uname)" == "Darwin" ]
 then
-    if [[ "$(clang-format --version)" != *4.0.1* ]]
+    if [[ "$(${CLANG_FORMAT} --version)" != *4.0.1* ]]
     then
         echo "automatic formatting on MAC OS X only available if clang-format 4.0.1";
         exit 2
     fi
 else
-    CLANG_VERSION=$(clang-format --version | sed -nre 's/^[^0-9]*(([0-9]+\.)*[0-9]+).*/\1/p' | tr -d '.' 2>/dev/null)
+    CLANG_VERSION=$(${CLANG_FORMAT} --version | sed -nre 's/^[^0-9]*(([0-9]+\.)*[0-9]+).*/\1/p' | tr -d '.' 2>/dev/null)
     if [[ "${CLANG_VERSION}" -lt 400 ]] || [[ "${CLANG_VERSION}" -gt 401 ]]
     then
         echo "automatic formatting only available if clang-format 4.0.0 or 4.0.1 are available";
         exit 2
     fi
 fi
+
+$CLANG_FORMAT --version
 
 echo "Formatting..."
 for FILE in $SOURCES
