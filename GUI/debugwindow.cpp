cd16183e64c95caaba672094b248ada789b59d69
alwina <alwina@lbl.gov>
2018-07-04 00:13:15 -0700

    fix formatting issues

--- GUI/debugwindow.cpp
+++ GUI/debugwindow.cpp
@@ -2,91 +2,79 @@
 #include "ui_debugwindow.h"
 
 #include "mainwindow.h"
-#include <map>
 #include <QFileDialog>
+#include <map>
 #include <vector>
 
-DebugWindow::DebugWindow(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::DebugWindow)
+DebugWindow::DebugWindow(QWidget *parent) : QDialog(parent), ui(new Ui::DebugWindow)
 {
-    ui->setupUi(this);
-    ui->scanSelector->addItem("Load config file first", -1);
+  ui->setupUi(this);
+  ui->scanSelector->addItem("Load config file first", -1);
 
-    connect(ui->startButton, SIGNAL(clicked()),
-    	this, SLOT(passSelectedScan()));
-    connect(ui->configLoadButton, SIGNAL(clicked()),
-        this, SLOT(selectAndLoadConfig()));
-    connect(this, SIGNAL(selectScan(TScanType)),
-    	this->parent(), SLOT(doDebugScan(TScanType)));
-    connect(this, SIGNAL(passConfigFile(QByteArray)),
-    	this->parent(), SLOT(loadConfigFile(QByteArray)));
-    connect(this->parent(), SIGNAL(deviceLoaded(TDeviceType)),
-    	this, SLOT(displayDeviceAndScans(TDeviceType)));
+  connect(ui->startButton, SIGNAL(clicked()), this, SLOT(passSelectedScan()));
+  connect(ui->configLoadButton, SIGNAL(clicked()), this, SLOT(selectAndLoadConfig()));
+  connect(this, SIGNAL(selectScan(TScanType)), this->parent(), SLOT(doDebugScan(TScanType)));
+  connect(this, SIGNAL(passConfigFile(QByteArray)), this->parent(),
+          SLOT(loadConfigFile(QByteArray)));
+  connect(this->parent(), SIGNAL(deviceLoaded(TDeviceType)), this,
+          SLOT(displayDeviceAndScans(TDeviceType)));
 }
 
-DebugWindow::~DebugWindow()
-{
-    delete ui;
-}
+DebugWindow::~DebugWindow() { delete ui; }
 
 void DebugWindow::selectAndLoadConfig()
 {
-    QString configFilename = QFileDialog::getOpenFileName(
-                this,
-                "Select config file",
-                QDir::currentPath(),
-                "All files (*.*)");
-    if (!configFilename.isNull()) {
-	    ui->configFileDisplay->setPlainText(configFilename);
-        emit passConfigFile(configFilename.toLatin1());
-    }
+  QString configFilename = QFileDialog::getOpenFileName(this, "Select config file",
+                                                        QDir::currentPath(), "All files (*.*)");
+  if (!configFilename.isNull()) {
+    ui->configFileDisplay->setPlainText(configFilename);
+    emit passConfigFile(configFilename.toLatin1());
+  }
 }
 
 void DebugWindow::displayDeviceAndScans(TDeviceType deviceType)
 {
-    ui->deviceDisplay->setText("Device type selected: " + DeviceNameMap.at(deviceType));
-	ui->scanSelector->clear(); 
-    switch(deviceType) {
-    	case TYPE_POWER:
-    		addPowerScans();
-    		break;
-    	default:
-    		addStandardScans();
-    		break;
-    }
+  ui->deviceDisplay->setText("Device type selected: " + DeviceNameMap.at(deviceType));
+  ui->scanSelector->clear();
+  switch (deviceType) {
+  case TYPE_POWER:
+    addPowerScans();
+    break;
+  default:
+    addStandardScans();
+    break;
+  }
 }
 
 void DebugWindow::addStandardScans()
 {
-	for(std::pair<TScanType, QString> scanIterator : ScanNameMap) {
-		ui->scanSelector->addItem(scanIterator.second, scanIterator.first);
-	}
+  for (std::pair<TScanType, QString> scanIterator : ScanNameMap) {
+    ui->scanSelector->addItem(scanIterator.second, scanIterator.first);
+  }
 }
 
 void DebugWindow::addPowerScans()
 {
-	std::vector<TScanType> scansForSetup;
-	scansForSetup.push_back(STFastPowerTest);
+  std::vector<TScanType> scansForSetup;
+  scansForSetup.push_back(STFastPowerTest);
 
-	displayScans(scansForSetup);
+  displayScans(scansForSetup);
 }
 
 void DebugWindow::displayScans(const std::vector<TScanType> scansForSetup)
 {
-	for(TScanType scanType : scansForSetup)
-	{
-		ui->scanSelector->addItem(ScanNameMap.at(scanType), scanType);
-	}
-
+  for (TScanType scanType : scansForSetup) {
+    ui->scanSelector->addItem(ScanNameMap.at(scanType), scanType);
+  }
 }
 
 void DebugWindow::passSelectedScan()
 {
-	int selectedScan = ui->scanSelector->currentData().toInt();
-	if (selectedScan == -1) {
-		std::cout << "Select a scan" << std::endl;
-	} else {
-		emit selectScan((TScanType) selectedScan);
-	}
+  int selectedScan = ui->scanSelector->currentData().toInt();
+  if (selectedScan == -1) {
+    std::cout << "Select a scan" << std::endl;
+  }
+  else {
+    emit selectScan((TScanType)selectedScan);
+  }
 }
--- GUI/debugwindow.h
+++ GUI/debugwindow.h
@@ -1,78 +1,65 @@
 #ifndef DEBUGWINDOW_H
 #define DEBUGWINDOW_H
 
-#include "mainwindow.h"
 #include "TConfig.h"
-#include <map>
+#include "mainwindow.h"
 #include <QDialog>
 #include <QString>
+#include <map>
 #include <vector>
 
 namespace Ui {
-class DebugWindow;
+  class DebugWindow;
 }
 
-class DebugWindow : public QDialog
-{
-    Q_OBJECT
+class DebugWindow : public QDialog {
+  Q_OBJECT
 
 public:
-    explicit DebugWindow(QWidget *parent = 0);
-    ~DebugWindow();
+  explicit DebugWindow(QWidget *parent = 0);
+  ~DebugWindow();
 
 public slots:
-    void displayDeviceAndScans(TDeviceType deviceType);
+  void displayDeviceAndScans(TDeviceType deviceType);
 
 signals:
-	void selectScan(TScanType scanType);
-    void passConfigFile(QByteArray configFilename);
+  void selectScan(TScanType scanType);
+  void passConfigFile(QByteArray configFilename);
 
 private:
-    Ui::DebugWindow *ui;
-    void addStandardScans();
-    void addPowerScans();
-    void displayScans(std::vector<TScanType> scansForSetup);
+  Ui::DebugWindow *ui;
+  void             addStandardScans();
+  void             addPowerScans();
+  void displayScans(std::vector<TScanType> scansForSetup);
 
-    const std::map<TScanType, QString> ScanNameMap = {
-        {STPower, "Power"},
-        {STFifo, "Fifo"},
-        {STLocalBus, "LocalBus"},
-        {STDigital, "Digital"},
-        {STDigitalWF, "DigitalWF"},
-        {STThreshold, "Threshold"},
-        {STVCASN, "VCASN"},
-        {STITHR, "ITHR"},
-        {STApplyITHR, "ApplyITHR"},
-        {STApplyVCASN, "ApplyVCASN"},
-        {STApplyMask, "ApplyMask"},
-        {STClearMask, "ClearMask"},
-        {STNoise, "Noise"},
-        {STReadout, "Readout"},
-        {STEndurance, "Endurance"},
-        {STFastPowerTest, "FastPowerTest"},
-        {STDctrl, "Dctrl"},
-        {STEyeScan, "EyeScan"}
-    };
+  const std::map<TScanType, QString> ScanNameMap = {
+      {STPower, "Power"},         {STFifo, "Fifo"},
+      {STLocalBus, "LocalBus"},   {STDigital, "Digital"},
+      {STDigitalWF, "DigitalWF"}, {STThreshold, "Threshold"},
+      {STVCASN, "VCASN"},         {STITHR, "ITHR"},
+      {STApplyITHR, "ApplyITHR"}, {STApplyVCASN, "ApplyVCASN"},
+      {STApplyMask, "ApplyMask"}, {STClearMask, "ClearMask"},
+      {STNoise, "Noise"},         {STReadout, "Readout"},
+      {STEndurance, "Endurance"}, {STFastPowerTest, "FastPowerTest"},
+      {STDctrl, "Dctrl"},         {STEyeScan, "EyeScan"}};
 
-    const std::map<TDeviceType, QString> DeviceNameMap = {
-        {TYPE_CHIP, "CHIP"},
-        {TYPE_TELESCOPE, "TELESCOPE"},
-        {TYPE_OBHIC, "OBHIC"},
-        {TYPE_IBHIC, "IBHIC"},
-        {TYPE_CHIP_MOSAIC, "CHIP_MOSAIC"},
-        {TYPE_HALFSTAVE, "HALFSTAVE"},
-        {TYPE_HALFSTAVERU, "HALFSTAVERU"},
-        {TYPE_MLHALFSTAVE, "MLHALFSTAVE"},
-        {TYPE_MLSTAVE, "MLSTAVE"},
-        {TYPE_IBHICRU, "IBHICRU"},
-        {TYPE_ENDURANCE, "ENDURANCE"},
-        {TYPE_POWER, "POWER"},
-        {TYPE_UNKNOWN, "UNKNOWN"}
-    };
+  const std::map<TDeviceType, QString> DeviceNameMap = {{TYPE_CHIP, "CHIP"},
+                                                        {TYPE_TELESCOPE, "TELESCOPE"},
+                                                        {TYPE_OBHIC, "OBHIC"},
+                                                        {TYPE_IBHIC, "IBHIC"},
+                                                        {TYPE_CHIP_MOSAIC, "CHIP_MOSAIC"},
+                                                        {TYPE_HALFSTAVE, "HALFSTAVE"},
+                                                        {TYPE_HALFSTAVERU, "HALFSTAVERU"},
+                                                        {TYPE_MLHALFSTAVE, "MLHALFSTAVE"},
+                                                        {TYPE_MLSTAVE, "MLSTAVE"},
+                                                        {TYPE_IBHICRU, "IBHICRU"},
+                                                        {TYPE_ENDURANCE, "ENDURANCE"},
+                                                        {TYPE_POWER, "POWER"},
+                                                        {TYPE_UNKNOWN, "UNKNOWN"}};
 
 private slots:
-    void passSelectedScan();
-    void selectAndLoadConfig();
+  void passSelectedScan();
+  void selectAndLoadConfig();
 };
 
 #endif // DEBUGWINDOW_H
