9e774938d6ed0766ba8e69455c2aab32a0220e4a
Felix Reidt <felix.reidt@cern.ch>
2018-07-27 09:06:28 +0000

    - if/else - 0x0 => nullptr

--- GUI/mainwindow.cpp
+++ GUI/mainwindow.cpp
@@ -996,8 +996,9 @@ void MainWindow::CombineEnduranceResults()
     TCycleResultHic *lastHicResult =
         (TCycleResultHic *)lastResult->GetHicResult(fHICs.at(ihic)->GetDbId());
     TCycleAnalysis *lastAnalysis = (TCycleAnalysis *)fAnalysisVector.at(lastEndurance);
-    if (lastHicResult == 0x0) popup("A problem was detected\nPlease check \nthe cable connections");
-    if (lastHicResult) lastAnalysis->ReClassify(lastHicResult);
+    if (lastHicResult == nullptr) 
+      popup("A problem was detected\nPlease check \nthe cable connections");
+    else lastAnalysis->ReClassify(lastHicResult);
   }
 }
 
