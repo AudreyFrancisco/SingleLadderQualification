b7593ead6e15bf8fc6bda1897659ae6ba7899132
rhanniga <ryan.hannigan@cern.ch>
2018-09-07 16:48:57 -0500

    fixed a typo that would have been terrible to find

--- RUv1Src/TRUv1GpioFrontend.cpp
+++ RUv1Src/TRUv1GpioFrontend.cpp
@@ -35,8 +35,8 @@ void TRUv1GpioFrontend::EnableAlignmentSingle(int trnum, bool enable, bool commi
 
 void TRUv1GpioFrontend::EnableDataSingle(int trnum, bool enable, bool commit)
 {
-  WriteMaskedRegSingle(trnum, TRUv1GpioFrontend::ENABLE_DATA_H,
-                       TRUv1GpioFrontend::ENABLE_DATA_L, enable, commit);
+  WriteMaskedRegSingle(trnum, TRUv1GpioFrontend::ENABLE_DATA_H, TRUv1GpioFrontend::ENABLE_DATA_L,
+                       enable, commit);
 }
 
 void TRUv1GpioFrontend::WriteMaskedReg(uint16_t address_h, uint16_t address_l, bool flag,
