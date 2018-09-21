41e8487ddda52d16f8250622752fb1169ce48790
Markus Keil <markus.keil@cern.ch>
2018-09-21 16:50:01 +0200

    DBHelpers modifications for comp analysis

--- DataBaseSrc/DBHelpers.h
+++ DataBaseSrc/DBHelpers.h
@@ -61,7 +61,7 @@ string CreateActivityName(string compName, TScanConfig *config);
 string GetServiceAccount(string institute, string &folder);
 string GetEosPath(ActivityDB::activityLong activity, THicType hicType, bool doubleComp);
 string GetTestDirName(TTestType TestType);
-TTestType GetTestType(string activityName);
+TTestType GetTestType(string activityTypeName);
 bool GetDigitalFileName(ActivityDB::activityLong activity, int chip, int voltPercent, int backBias,
                         string &dataName, string &resultName);
 #endif
